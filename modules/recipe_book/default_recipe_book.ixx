fc804a36c0ab0f12025241e0229550e191638a5d
erickswong <erickswong@hotmail.com>
2022-06-26 06:39:34 -0700

    Reorder default_recipe_book

--- modules/recipe_book/default_recipe_book.ixx
+++ modules/recipe_book/default_recipe_book.ixx
@@ -10,44 +10,4 @@ namespace recipe_book::default_recipe_book {
 	void addCloths();
 	void addLeathers();
 	void addBlocks();
-
-	void addIngots();
-	void addPlanks();
-	void addCloths();
-	void addLeathers();
-	void addBlocks();
-
-	void addCharcoal();
-	void addIronIngot();
-	void addSteelIngot();
-	void addStarmetalIngot();
-	void addOrichalcumIngot();
-	void addSilverIngot();
-	void addGoldIngot();
-	void addPlatinumIngot();
-	void addAsmodeum();
-
-	void addTimber();
-	void addLumber();
-	void addWyrdwoodPlanks();
-	void addIronwoodPlanks();
-	void addGlitteringEbony();
-
-	void addLinen();
-	void addSateen();
-	void addSilk();
-	void addInfusedSilk();
-	void addPhoenixweave();
-
-	void addCoarseLeather();
-	void addRuggedLeather();
-	void addLayeredLeather();
-	void addInfusedLeather();
-	void addRunicLeather();
-
-	void addStoneBlock();
-	void addStoneBrick();
-	void addLodestoneBrick();
-	void addObsidianVoidstone();
-	void addRunestone();
 };
--- src/recipe_book/default_recipe_book.cpp
+++ src/recipe_book/default_recipe_book.cpp
@@ -18,70 +18,32 @@ namespace recipe_book::default_recipe_book {
 	}
 
 	void addIngots() {
-		addCharcoal();
-		addIronIngot();
-		addSteelIngot();
-		addStarmetalIngot();
-		addOrichalcumIngot();
-		addSilverIngot();
-		addGoldIngot();
-		addPlatinumIngot();
-		addAsmodeum();
-	}
-
-	void addPlanks() {
-		addTimber();
-		addLumber();
-		addWyrdwoodPlanks();
-		addIronwoodPlanks();
-		addGlitteringEbony();
-	}
-
-	void addCloths() {
-		addLinen();
-		addSateen();
-		addSilk();
-		addInfusedSilk();
-		addPhoenixweave();
-	}
-
-	void addLeathers() {
-		addCoarseLeather();
-		addRuggedLeather();
-		addLayeredLeather();
-		addInfusedLeather();
-		addRunicLeather();
-	}
-
-	void addBlocks() {
-		addStoneBlock();
-		addStoneBrick();
-		addLodestoneBrick();
-		addObsidianVoidstone();
-		addRunestone();
-	}
-
-	void addCharcoal() {
-		addRecipe(Recipe("Charcoal").addIngredient("Green Wood", 2));
-		addRecipe(Recipe("Charcoal").addIngredient("Aged Wood", 2));
-		addRecipe(Recipe("Charcoal").addIngredient("Wyrdwood", 2));
-		addRecipe(Recipe("Charcoal").addIngredient("Ironwood", 2));
-	}
-
-	void addIronIngot() {
-		addRecipe(Recipe("Iron Ingot").addIngredient("Iron Ore", 4));
-	}
-
-	void addSteelIngot() {
+		addRecipe(
+			Recipe("Charcoal")
+			.addIngredient("Green Wood", 2)
+		);
+		addRecipe(
+			Recipe("Charcoal")
+			.addIngredient("Aged Wood", 2)
+		);
+		addRecipe(
+			Recipe("Charcoal")
+			.addIngredient("Wyrdwood", 2)
+		);
+		addRecipe(
+			Recipe("Charcoal")
+			.addIngredient("Ironwood", 2)
+		);
+		addRecipe(
+			Recipe("Iron Ingot")
+			.addIngredient("Iron Ore", 4)
+		);
 		addRecipe(
 			Recipe("Steel Ingot")
 			.addIngredient("Iron Ingot", 3)
 			.addIngredient("Charcoal", 2)
 			.addIngredient("Obsidian Flux", 1)
 		);
-	}
-
-	void addStarmetalIngot() {
 		addRecipe(
 			Recipe("Starmetal Ingot")
 			.addIngredient("Starmetal Ore", 6)
@@ -89,9 +51,6 @@ namespace recipe_book::default_recipe_book {
 			.addIngredient("Charcoal", 2)
 			.addIngredient("Obsidian Flux", 1)
 		);
-	}
-
-	void addOrichalcumIngot() {
 		addRecipe(
 			Recipe("Orichalcum Ingot")
 			.addIngredient("Orichalcum Ore", 8)
@@ -106,31 +65,22 @@ namespace recipe_book::default_recipe_book {
 			.addIngredient("Charcoal", 2)
 			.addIngredient("Obsidian Flux", 1)
 		);
-	}
-
-	void addSilverIngot() {
-		addRecipe(Recipe("Silver Ingot").addIngredient("Silver Ore", 4));
-	}
-
-	void addGoldIngot() {
+		addRecipe(
+			Recipe("Silver Ingot")
+			.addIngredient("Silver Ore", 4)
+		);
 		addRecipe(
 			Recipe("Gold Ingot")
 			.addIngredient("Gold Ore", 5)
 			.addIngredient("Silver Ingot", 2)
 			.addIngredient("Obsidian Flux", 1)
 		);
-	}
-
-	void addPlatinumIngot() {
 		addRecipe(
 			Recipe("Platinum Ingot")
 			.addIngredient("Platinum Ore", 6)
 			.addIngredient("Gold Ingot", 2)
 			.addIngredient("Obsidian Flux", 1)
 		);
-	}
-
-	void addAsmodeum() {
 		addRecipe(
 			Recipe("Asmodeum")
 			.addIngredient("Orichalcum Ingot", 5)
@@ -141,38 +91,29 @@ namespace recipe_book::default_recipe_book {
 		);
 	}
 
-	void addTimber() {
-		addRecipe(Recipe("Timber").addIngredient("Green Wood", 4));
-	}
-
-	void addLumber() {
+	void addPlanks() {
+		addRecipe(
+			Recipe("Timber")
+			.addIngredient("Green Wood", 4)
+		);
 		addRecipe(
 			Recipe("Lumber")
 			.addIngredient("Aged Wood", 4)
 			.addIngredient("Timber", 2)
 			.addIngredient("Obsidian Sandpaper", 1)
 		);
-	}
-
-	void addWyrdwoodPlanks() {
 		addRecipe(
 			Recipe("Wyrdwood Planks")
 			.addIngredient("Wyrdwood", 6)
 			.addIngredient("Lumber", 2)
 			.addIngredient("Obsidian Sandpaper", 1)
 		);
-	}
-
-	void addIronwoodPlanks() {
 		addRecipe(
 			Recipe("Ironwood Planks")
 			.addIngredient("Ironwood", 8)
 			.addIngredient("Wyrdwood Planks", 2)
 			.addIngredient("Obsidian Sandpaper", 1)
 		);
-	}
-
-	void addGlitteringEbony() {
 		addRecipe(
 			Recipe("Glittering Ebony")
 			.addIngredient("Ironwood Planks", 5)
@@ -182,37 +123,28 @@ namespace recipe_book::default_recipe_book {
 		);
 	}
 
-	void addLinen() {
-		addRecipe(Recipe("Linen").addIngredient("Fibers", 4));
-	}
-
-	void addSateen() {
+	void addCloths() {
+		addRecipe(
+			Recipe("Linen")
+			.addIngredient("Fibers", 4)
+		);
 		addRecipe(
 			Recipe("Sateen")
 			.addIngredient("Linen", 4)
 			.addIngredient("Wireweave", 1)
 		);
-	}
-
-	void addSilk() {
 		addRecipe(
 			Recipe("Silk")
 			.addIngredient("Silk Threads", 6)
 			.addIngredient("Sateen", 2)
 			.addIngredient("Wireweave", 1)
 		);
-	}
-
-	void addInfusedSilk() {
 		addRecipe(
 			Recipe("Infused Silk")
 			.addIngredient("Wirefiber", 8)
 			.addIngredient("Silk", 2)
 			.addIngredient("Wireweave", 1)
 		);
-	}
-
-	void addPhoenixweave() {
 		addRecipe(
 			Recipe("Phoenixweave")
 			.addIngredient("Infused Silk", 5)
@@ -222,37 +154,28 @@ namespace recipe_book::default_recipe_book {
 		);
 	}
 
-	void addCoarseLeather() {
-		addRecipe(Recipe("Coarse Leather").addIngredient("Rawhide", 4));
-	}
-
-	void addRuggedLeather() {
+	void addLeathers() {
+		addRecipe(
+			Recipe("Coarse Leather")
+			.addIngredient("Rawhide", 4)
+		);
 		addRecipe(
 			Recipe("Rugged Leather")
 			.addIngredient("Coarse Leather", 4)
 			.addIngredient("Aged Tannin", 1)
 		);
-	}
-
-	void addLayeredLeather() {
 		addRecipe(
 			Recipe("Layered Leather")
 			.addIngredient("Thick Hide", 6)
 			.addIngredient("Rugged Leather", 2)
 			.addIngredient("Aged Tannin", 1)
 		);
-	}
-
-	void addInfusedLeather() {
 		addRecipe(
 			Recipe("Infused Leather")
 			.addIngredient("Iron Hide", 8)
 			.addIngredient("Layered Leather", 2)
 			.addIngredient("Aged Tannin", 1)
 		);
-	}
-
-	void addRunicLeather() {
 		addRecipe(
 			Recipe("Runic Leather")
 			.addIngredient("Infused Leather", 5)
@@ -262,37 +185,28 @@ namespace recipe_book::default_recipe_book {
 		);
 	}
 
-	void addStoneBlock() {
-		addRecipe(Recipe("Stone Block").addIngredient("Stone", 4));
-	}
-
-	void addStoneBrick() {
+	void addBlocks() {
+		addRecipe(
+			Recipe("Stone Block")
+			.addIngredient("Stone", 4)
+		);
 		addRecipe(
 			Recipe("Stone Brick")
 			.addIngredient("Stone Block", 4)
 			.addIngredient("Obsidian Sandpaper", 1)
 		);
-	}
-
-	void addLodestoneBrick() {
 		addRecipe(
 			Recipe("Lodestone Brick")
 			.addIngredient("Lodestone", 6)
 			.addIngredient("Stone Brick", 2)
 			.addIngredient("Obsidian Sandpaper", 1)
 		);
-	}
-
-	void addObsidianVoidstone() {
 		addRecipe(
 			Recipe("Obsidian Voidstone")
 			.addIngredient("Lodestone Brick", 8)
 			.addIngredient("Lodestone", 2)
 			.addIngredient("Obsidian Sandpaper", 1)
 		);
-	}
-
-	void addRunestone() {
 		addRecipe(
 			Recipe("Runestone")
 			.addIngredient("Obsidian Voidstone", 5)
